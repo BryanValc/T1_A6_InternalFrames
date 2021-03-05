229ddb8ef056061b26cbaccc30ee2d37614a35a4
Bryan Valdez <bryan501th@gmail.com>
2021-03-05 16:00:07 -0600

    Se añadieron los menus en el menuBar, se creo un JIntermnalFrame llamado record que se mostró en pantalla

--- src/PruebaInternalframes.java
+++ src/PruebaInternalframes.java
@@ -27,15 +27,62 @@ class Ventana extends JFrame{
 	
 	JMenuBar menuBar;
 	JMenu masters,booking,testPerform,printing,transaction,labReports,settings,utilities,window,help;
+	JMenuItem menuItemReg;
+	JInternalFrame record;
 	
 	public Ventana() {
 		
-		getContentPane().setLayout(new BorderLayout());
+		getContentPane().setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(1080,630);
 		setLocationRelativeTo(null);
 		setTitle("InternalFrames");
 		setVisible(true);
+		
+		menuBar = new JMenuBar();
+
+			masters = new JMenu("Master");
+				menuItemReg= new JMenuItem("Record");
+				masters.add(menuItemReg);
+				menuItemReg.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						record.setVisible(true);
+						
+					}
+				});
+				
+			booking = new JMenu("Booking");
+			testPerform = new JMenu("Test Perform");
+			transaction = new JMenu("Transaction");
+			labReports = new JMenu("Lab Reports");
+			settings = new JMenu("Settings");
+			window = new JMenu("Window");
+			help = new JMenu("Help");
+			
+		menuBar.add(masters);
+		menuBar.add(booking);
+		menuBar.add(testPerform);
+		menuBar.add(transaction);
+		menuBar.add(labReports);
+		menuBar.add(settings);
+		menuBar.add(window);
+		menuBar.add(help);
+		setJMenuBar(menuBar);
+		
+		JDesktopPane dp = new JDesktopPane();
+		
+		record = new JInternalFrame();
+		record.getContentPane().setLayout(null);
+		record.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		record.setSize(1080, 630);
+		record.setTitle("Record");
+		
+		
+		dp.add(record);
+		dp.setBounds(0, 0, 1080, 630);
+		
+		add(dp);
 		
 	}
 	
